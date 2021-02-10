3acafd256f7e7bba6f0d4cf369ab11b829715832
Hussein Awala <houssein.awala.96@gmail.com>
2021-02-10 20:22:21 +0100

    fix a bug

--- app/database/connection.py
+++ app/database/connection.py
@@ -5,7 +5,7 @@ USERNAME = getenv("USERNAME", "fastapi")
 PASSWORD = getenv("PASSWORD", "fastapi")
 DATABASE_HOST = getenv("DB_HOST", "fastapi")
 DATABASE_NAME = getenv("DB_NAME", "fastapi")
-MONGO_URI = "mongodb+srv://{}:{}@/{}?retryWrites=true&w=1".format(USERNAME, PASSWORD, DATABASE_HOST, DATABASE_NAME)
+MONGO_URI = "mongodb+srv://{}:{}@{}/{}?retryWrites=true&w=1".format(USERNAME, PASSWORD, DATABASE_HOST, DATABASE_NAME)
 
 client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URI)
 
